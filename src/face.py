1db8507907820e61d822e3decbf44bc6c8b099e2
vladimir.fodorow@gmail.com <vladimir.fodorow@gmail.com>
2021-06-05 13:29:12 +0700

    Создание заготовки под интерфейс

---
+++ src/face.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+def main():
+    window = Tk()
+    window.title("Расчет толщины утеплителя для 3D принтера")
+    window.geometry('500x500')
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
