c84d2c4582617f6d792bfaa473ef043089fd2cb0
vladimir.fodorow@gmail.com <vladimir.fodorow@gmail.com>
2021-06-05 14:10:09 +0700

    Добавлен выпадающий список регионов

--- src/face.py
+++ src/face.py
@@ -1,10 +1,19 @@
 from tkinter import *
+from tkinter import ttk
+
+from src.core import get_regions_list
+
+regions = get_regions_list()
 
 
 def main():
     window = Tk()
     window.title("Расчет толщины утеплителя для 3D принтера")
     window.geometry('500x500')
+
+    combobox = ttk.Combobox(window, values=sorted(list(regions)), width=30)
+    combobox.place(x=10, y=10)
+
     window.mainloop()
 
 
