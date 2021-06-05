573137dfdc86e0aba9d46bdff98733889daac24c
vladimir.fodorow@gmail.com <vladimir.fodorow@gmail.com>
2021-06-05 14:24:44 +0700

    Добавлен выпадающий список с материалами

--- src/face.py
+++ src/face.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 
-from src.core import get_regions_list
+from src.core import get_regions_list, get_materials_list
 
 regions = get_regions_list()
+materials = get_materials_list()
 
 
 def main():
@@ -11,8 +12,11 @@ def main():
     window.title("Расчет толщины утеплителя для 3D принтера")
     window.geometry('500x500')
 
-    combobox = ttk.Combobox(window, values=sorted(list(regions)), width=30)
-    combobox.place(x=10, y=10)
+    regions_combobox = ttk.Combobox(window, values=sorted(list(regions)), width=30)
+    regions_combobox.place(x=10, y=10)
+
+    materials_combobox = ttk.Combobox(window, values=sorted(list(materials)), width=40)
+    materials_combobox.place(x=10, y=40)
 
     window.mainloop()
 
