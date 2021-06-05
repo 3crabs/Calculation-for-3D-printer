8594d819fd775d258fb4f121a25c84356468d062
vladimir.fodorow@gmail.com <vladimir.fodorow@gmail.com>
2021-06-05 14:52:33 +0700

    Добавлена фильтрация городов по выбраному региону

--- src/face.py
+++ src/face.py
@@ -6,17 +6,34 @@ from src.core import get_regions_list, get_materials_list
 regions = get_regions_list()
 materials = get_materials_list()
 
+window = Tk()
+regions_combobox = ttk.Combobox(window, values=sorted(list(regions)), width=30, state='readonly')
+city_combobox = ttk.Combobox(window, values=sorted(list(regions)), width=30, state='readonly')
+materials_combobox = ttk.Combobox(window, values=sorted(list(materials)), width=40, state='readonly')
+
+
+def hi(arg):
+    cities = regions[regions_combobox.get()]['cities']
+    c = []
+    for city in cities:
+        c.append(city['city'])
+    city_combobox['values'] = c
+    city_combobox.set('')
+    city_combobox["state"] = 'readonly'
+
 
 def main():
-    window = Tk()
+    global window, regions_combobox, city_combobox, materials_combobox
     window.title("Расчет толщины утеплителя для 3D принтера")
     window.geometry('500x500')
 
-    regions_combobox = ttk.Combobox(window, values=sorted(list(regions)), width=30)
     regions_combobox.place(x=10, y=10)
+    regions_combobox.bind("<<ComboboxSelected>>", hi)
 
-    materials_combobox = ttk.Combobox(window, values=sorted(list(materials)), width=40)
-    materials_combobox.place(x=10, y=40)
+    city_combobox.place(x=10, y=40)
+    city_combobox["state"] = DISABLED
+
+    materials_combobox.place(x=10, y=70)
 
     window.mainloop()
 
